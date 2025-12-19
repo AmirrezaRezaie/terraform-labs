6aaef073e645f218f4db76c74dc36adcfb152c3c
Amirreza Rezaie <82rezaeei@gmail.com>
2025-12-19 19:36:51 +0330

    Configure Vagrantfile

--- labs/vagrant-netdata/terrafrom/main.tf
+++ labs/vagrant-netdata/terrafrom/main.tf
@@ -2,17 +2,4 @@ provider "vagrant" {}
 
 resource "vagrant_vm" "netdata_vm" {
   name = "netdata-vm"
-
-  vagrantfile = <<-EOF
-    Vagrant.configure("2") do |config|
-      config.vm.box = "ubuntu/jammy64"
-
-      config.vm.provider "virtualbox" do |vb|
-        vb.memory = 1024
-        vb.cpus  = 1
-      end
-
-      config.vm.network "private_network", ip: "192.168.56.10"
-    end
-  EOF
 }
